8e58875feb1e154a2fe459bc99b474bde8bd42b6
Nikita <neonik108@gmail.com>
2022-07-02 21:28:07 +0300

    added "calculator"

--- Test_Luskovich_Repo/src/FirstExample/Case1.java
+++ Test_Luskovich_Repo/src/FirstExample/Case1.java
@@ -1,2 +1,37 @@
-package FirstExample;public class Case1 {
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        char operator;
+        Double First, Second, result;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose an operator: +, -, *, /");
+        operator = input.next().charAt(0);
+        System.out.println("Enter first number");
+        First = input.nextDouble();
+        System.out.println("Enter second number");
+        Second = input.nextDouble();
+        switch (operator) {
+
+            case '+':
+                result = First + Second;
+                System.out.println(First + " + " + Second + " = " + result);
+                break;
+            case '-':
+                result = First - Second;
+                System.out.println(First + " - " + Second + " = " + result);
+                break;
+            case '*':
+                result = First * Second;
+                System.out.println(First + " * " + Second + " = " + result);
+                break;
+            case '/':
+                result = First / Second;
+                System.out.println(First + " / " + Second + " = " + result);
+                break;
+
+        }
+    }
 }
+
