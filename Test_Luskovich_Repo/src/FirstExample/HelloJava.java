9c7aca229e8dc5425fd45f6201be8807c6563ee2
Nikita <neonik108@gmail.com>
2022-07-02 20:32:45 +0300

    added "Hello Java 18"

---
+++ Test_Luskovich_Repo/src/FirstExample/HelloJava.java
@@ -0,0 +1,10 @@
+package FirstExample;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        var Java = "Hello Java! ";
+        var Version = 18;
+        System.out.println(Java + Version);
+
+    }
+}
