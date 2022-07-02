8e21b65959b4ae49e42ceca6775b78a43514e28a
Nikita <neonik108@gmail.com>
2022-07-02 21:41:56 +0300

    A new file

---
+++ Test_Luskovich_Repo/src/FirstExample/PracticeAdd.java
@@ -0,0 +1,11 @@
+package FirstExample;
+
+import java.util.Arrays;
+
+public class PracticeAdd {
+    public static void main(String[] args) {
+        var U = "Using";
+        var A = "Added";
+        System.out.println( U + A );
+    }
+}
